044f1e4d775ad24c15b3f728647a1541ad66d585
neonhdz <angelhdzlazc@gmail.com>
2025-08-31 22:37:39 -0600

    FuncionesLambda v 1.0

---
+++ EDAT_JD25_P01/FuncionesLambda/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Expresiones Lambda como funcion");
+        //Func<int, int , int >b=(a)=>a*2;
+        //int resultado =b(4);
+        //Console.WriteLine("El resultado es: "+resultado);
+        //Func<int, int, int> suma=(a,b)=>a+b;
+        //int resultado = suma(4,6);
+        //Console.WriteLine("El resultado es: " + resultado);
+        //Func<int, int, int> mayor = (a, b) =>
+        //{
+        //    if (a > b) return a;
+        //    else return b;
+        //};
+        //int resultado = mayor(45,20);
+        //Console.WriteLine("El numero mayor es: " + resultado);
+        //Persona pers = new Persona();
+        //int edad=18;
+        //edad++; 
+        //edad++;
+        //Console.ReadKey();
+        //Console.WriteLine("El valor de la variable edad es: "+edad);
+        //Console.WriteLine("La direccion de memoria es: " + ((IntPtr)edad).ToString("X"));
+        var numeros = new List<int> { 8, 5, 9, 34, 8 };
+        Func<int, bool> ObtenerPares = num => num % 2 == 0;
+        var pares = numeros.Where(ObtenerPares);
+        Console.WriteLine("Numeros pares:");
+        foreach (var item in pares)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}

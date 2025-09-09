a139c843146b9a3f6f574df9769cefa95205cd9f
neonhdz <angelhdzlazc@gmail.com>
2025-09-08 23:35:24 -0600

    Tarea 01. Práctica - Ejercicios de recursividad

---
+++ EDAT_JD25_P01/BibliotecaTarea01Recursividad/RecursividadContenido.cs
@@ -0,0 +1,38 @@
+﻿namespace BibliotecaTarea01Recursividad;
+
+public class RecursividadContenido
+{
+
+    // 1. Cantidad de vocales en una cadena
+    public int vocals(string chain)
+    {
+        if (chain.Length == 0)
+        {
+            return 0;
+        }
+        char first = char.ToLower(chain[0]);
+        int vocal = (first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') ? 1 : 0;
+
+        return vocal + vocals(chain.Substring(1));
+    }
+    // 2. Suma de dígitos de un número
+    public int suma1(int num1)
+    {
+        if (num1 < 10)
+        {
+            return num1;
+        }
+
+        return (num1 % 10) + suma1(num1 / 10);
+    }
+    // 3. Suma de los primeros n números
+    public int suma2(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        return suma2(n - 1) + n;
+    }
+}
---
+++ EDAT_JD25_P01/ConsolaTarea01Recursividad/Program.cs
@@ -0,0 +1,34 @@
+﻿using BibliotecaTarea01Recursividad;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        BibliotecaTarea01Recursividad.RecursividadContenido r = new BibliotecaTarea01Recursividad.RecursividadContenido();
+
+        // 1. Cantidad de vocales en una cadena
+        Console.WriteLine("_______________________________________________________________________________\n");
+        Console.WriteLine("Ingrese una cadena de texto:");
+        string chain = Console.ReadLine();
+        int cantidadVocales = r.vocals(chain);
+        Console.WriteLine("La cantidad de vocales en la cadena -" + chain + "- es: " + cantidadVocales);
+        Console.WriteLine("_______________________________________________________________________________\n");
+        // 2. Suma de dígitos de un número
+
+        Console.WriteLine("Ingrese un número (1 dígito o más):");
+        int num = int.Parse(Console.ReadLine());
+        int sum = r.suma1(num);
+        Console.WriteLine("La suma de los dígitos del número -" + num + "- es: " + sum);
+        Console.WriteLine("_______________________________________________________________________________\n");
+        // 3. Suma de los primeros n números
+        Console.WriteLine("_______________________________________________________________________________\n");
+        Console.WriteLine("Ingrese un número:");
+        int n = int.Parse(Console.ReadLine());
+        int sum2 = r.suma2(n);
+        Console.WriteLine("La suma de los primeros números hasta -" + n + "- es: " + sum2);
+        Console.WriteLine("_______________________________________________________________________________\n");
+        Console.WriteLine("Esperando una tecla...\n");
+        Console.ReadKey();
+    }
+}
